5c1ec77773bf488156e7e13a9b769bc30dfde171
CSY3074132838 <161829448+CSY3074132838@users.noreply.github.com>
2024-12-03 19:29:11 +0800

    Add files via upload

--- src/Ride.java
+++ src/Ride.java
@@ -1,14 +1,75 @@
-public class Ride {
-    private String rideName;
-    private boolean isOpen;
-    private Employee operator;
+import java.util.LinkedList;
+import java.util.List;
 
-    public Ride() {}
+public class Ride implements RideInterface {
+    private String rideName;  
+    private boolean isOpen;   
+    private Employee operator; 
+    private List<Visitor> queue;  
+    private List<Visitor> rideHistory;  
+
+    public Ride() {
+        this.queue = new LinkedList<>();
+        this.rideHistory = new LinkedList<>();
+    }
 
     public Ride(String rideName, boolean isOpen, Employee operator) {
+        this();
         this.rideName = rideName;
         this.isOpen = isOpen;
         this.operator = operator;
+    }
+
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        queue.add(visitor);
+    }
+
+    @Override
+    public void removeVisitorFromQueue(Visitor visitor) {
+        queue.remove(visitor);
+    }
+
+    @Override
+    public void printQueue() {
+        System.out.println("Visitor queues：");
+        for (Visitor visitor : queue) {
+            System.out.println(visitor.getName() + " (" + visitor.getTicketNumber() + ")");
+        }
+    }
+
+    @Override
+    public void runOneCycle() {
+        if (isOpen && !queue.isEmpty()) {
+            Visitor visitor = queue.remove(0);  
+            System.out.println(visitor.getName() + " Experiencing " + rideName);
+            addVisitorToHistory(visitor);  
+        } else {
+            System.out.println("Rides are not open or queues are empty for operation.");
+        }
+    }
+
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+        rideHistory.add(visitor);
+    }
+
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visitor) {
+        return rideHistory.contains(visitor);
+    }
+
+    @Override
+    public int numberOfVisitors() {
+        return rideHistory.size();
+    }
+
+    @Override
+    public void printRideHistory() {
+        System.out.println("History of Amusement Rides：");
+        for (Visitor visitor : rideHistory) {
+            System.out.println(visitor.getName() + " (" + visitor.getTicketNumber() + ")");
+        }
     }
 
     public String getRideName() {
@@ -35,3 +96,4 @@ public class Ride {
         this.operator = operator;
     }
 }
+
