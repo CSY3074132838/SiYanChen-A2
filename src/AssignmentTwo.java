673c13c972889a21f43a81a39d923ad164e6fc1c
CSY3074132838 <161829448+CSY3074132838@users.noreply.github.com>
2024-12-04 20:43:24 +0800

    Add files via upload

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,71 +1,92 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
-        System.out.println("Part Three：Create objects for employees and visitors and display their information");
+        System.out.println("Part IV B: Sort visitors by name and ticket number");
 
-        Employee employee = new Employee("Max Harrison", 30, "32 George Street, The Rocks, Sydney, NSW 2000", "E123", "Driver");
+        Employee operator = new Employee("Liam Williams", 35, "5 Martin Place, Sydney, NSW 2000", "E789", "caretaker");
+        Ride ride = new Ride("Roller Coaster", true, operator);
+
         Visitor visitor1 = new Visitor("Charlotte Johnson", 25, "57 Pitt Street, Sydney, NSW 2000", "V456", "Annual Pass for Amusement Parks");
         Visitor visitor2 = new Visitor("Jackson Davis", 22, "33 Macquarie Street, Circular Quay, Sydney, NSW 2000", "V202", "general admission ticket");
         Visitor visitor3 = new Visitor("Ava Brown", 28, "12 Crown Street, Surry Hills, Sydney, NSW 2010", "V303", "VIP ticket");
         Visitor visitor4 = new Visitor("Ethan Thompson", 40, "42 Acacia Street, Bondi Beach, New South Wales, 2026", "V404", "One-day Pass");
         Visitor visitor5 = new Visitor("Liam Murphy", 18, "105 Ocean Boulevard, Gold Coast, Queensland, 4217", "V505", "VIP");
 
-        System.out.println("Worker: " + employee.getName() + ", " + employee.getPosition());
-        System.out.println("Visitor: " + visitor1.getName() + ", " + visitor1.getMembershipType());
+        ride.addVisitorToHistory(visitor1);
+        ride.addVisitorToHistory(visitor2);
+        ride.addVisitorToHistory(visitor3);
+        ride.addVisitorToHistory(visitor4);
+        ride.addVisitorToHistory(visitor5);
 
-        System.out.println("Part IVA: Display of the state of the rides");
+        System.out.println("Historical records of the original rides:");
+        ride.printRideHistory();
 
-        Employee operator = new Employee("Liam Williams", 35, "5 Martin Place, Sydney, NSW 2000", "E789", "caretaker");
-        Ride ride = new Ride("roller coaster", true, operator);
-        System.out.println("Name of Amusement Ride: " + ride.getRideName() + ", Open or not: " + (ride.isOpen() ? "True" : "False"));
+        ride.sortRideHistory();
 
-        ride.addVisitorToQueue(visitor1);
-        ride.addVisitorToQueue(visitor2);
-        ride.addVisitorToQueue(visitor3);
-        ride.addVisitorToQueue(visitor4);
-        ride.addVisitorToQueue(visitor5);
-        System.out.println("Added 5 visitors to the queue.");
+        System.out.println("\nSorted ride history:");
+        ride.printRideHistory();
 
-        ride.printQueue();
+        System.out.println("\nPart III: Creating Employee and Visitor Objects and Displaying Their Information");
 
+        Employee employee = new Employee("Max Harrison", 30, "32 George Street, The Rocks, Sydney, NSW 2000", "E123", "Driver");
+        System.out.println("employees: " + employee.getName() + ", positions: " + employee.getPosition());
 
-        ride.runOneCycle();
-        
-        ride.removeVisitorFromQueue(visitor3);
-        System.out.println("Removed visitor " + visitor3.getName() + " from the queue.");
+        Visitor visitor = new Visitor("Charlotte Johnson", 25, "57 Pitt Street, Sydney, NSW 2000", "V456", "Annual Pass for Amusement Parks");
+        System.out.println("visitor: " + visitor.getName() + ", membership Type: " + visitor.getMembershipType());
 
-        ride.printQueue();
+        System.out.println("\nPart IVA: Display the status of the rides");
 
-        System.out.println("Part IVB: Displaying Employee Information");
+        Employee operatorForRide = new Employee("Liam Williams", 35, "5 Martin Place, Sydney, NSW 2000", "E789", "caretaker");
+        Ride rideForStatus = new Ride("Roller Coaster", true, operatorForRide);
+        System.out.println("Name of Amusement Ride: " + rideForStatus.getRideName() + ", open or not: " + (rideForStatus.isOpen() ? "True" : "False"));
+
+        rideForStatus.addVisitorToQueue(visitor1);
+        rideForStatus.addVisitorToQueue(visitor2);
+        rideForStatus.addVisitorToQueue(visitor3);
+        rideForStatus.addVisitorToQueue(visitor4);
+        rideForStatus.addVisitorToQueue(visitor5);
+        System.out.println("5 visitors have been added to the queue.");
+
+        rideForStatus.printQueue();
+
+        rideForStatus.runOneCycle();
+
+        rideForStatus.removeVisitorFromQueue(visitor3);
+        System.out.println("visiotr " + visitor3.getName() + " removed from queue.");
+
+        rideForStatus.printQueue();
+
+        System.out.println("\nPart IV B: Displaying Employee Information");
 
         Employee employee2 = new Employee("Ava Brown", 28, "12 Crown Street, Surry Hills, Sydney, NSW 2010", "E101", "maintenance man");
-        System.out.println("Employee Name: " + employee2.getName() + ", appointment: " + employee2.getPosition());
+        System.out.println("Employee Name: " + employee2.getName() + ", position: " + employee2.getPosition());
 
-        System.out.println("Section V: Display of visitor information");
+        System.out.println("\nPart V: Displaying Visitor Information");
 
         Visitor visitor6 = new Visitor("Jackson Davis", 22, "33 Macquarie Street, Circular Quay, Sydney, NSW 2000", "V202", "general admission ticket");
-        System.out.println("Visitor Name: " + visitor6.getName() + ", membershipType: " + visitor6.getMembershipType());
+        System.out.println("Visitor name: " + visitor6.getName() + ", Membership Type: " + visitor6.getMembershipType());
 
-        System.out.println("Part VI: Modelling the operation of an amusement ride");
+        System.out.println("\nPart VI: Operation of simulated rides");
 
-        Employee operator2 = new Employee("Noah Wilson", 32, "40 Eliza Street, Newtown, Sydney, NSW 2042", "E303", "operator");
-        Ride ride2 = new Ride("water rafting", false, operator2);
-        System.out.println("Name of Amusement Ride: " + ride2.getRideName() + ", Open or not: " + (ride2.isOpen() ? "True" : "False"));
+        Employee operatorForRide2 = new Employee("Noah Wilson", 32, "40 Eliza Street, Newtown, Sydney, NSW 2042", "E303", "operator");
+        Ride rideForOperation = new Ride("Water Rafting", false, operatorForRide2);
+        System.out.println("Name of Amusement Rides: " + rideForOperation.getRideName() + ", open or not: " + (rideForOperation.isOpen() ? "True" : "False"));
 
-        ride2.runOneCycle();
-        ride2.printQueue();
+        rideForOperation.runOneCycle();
+        rideForOperation.printQueue();
 
-        System.out.println("Part VII: Demonstrating Complete Employee-Visitor Interaction");
+        System.out.println("\nPart 7: Complete Employee-Visitor Interaction Presentation");
 
-        Employee employee3 = new Employee("Ethan Thompson", 40, "42 Acacia Street, Bondi Beach, New South Wales, 2026, Australia", "E404", "supervisors");
-        Visitor visitor7 = new Visitor("Liam Murphy", 18, "105 Ocean Boulevard, Gold Coast, Queensland, 4217, Australia", "V505", "VIP");
-        System.out.println("Staff interaction with visitors: " + employee3.getName() + " and " + visitor7.getName() + " are talking to each other.");
+        Employee employee3 = new Employee("Ethan Thompson", 40, "42 Acacia Street, Bondi Beach, New South Wales, 2026", "E404", "supervisor");
+        Visitor visitor7 = new Visitor("Liam Murphy", 18, "105 Ocean Boulevard, Gold Coast, Queensland, 4217", "V505", "VIP");
+        System.out.println("Staff and visitor interaction: " + employee3.getName() + " and " + visitor7.getName() + " are talking.");
 
-        ride.addVisitorToQueue(visitor7);
-        ride.printQueue();
+        rideForOperation.addVisitorToQueue(visitor7);
+        rideForOperation.printQueue();
 
-        ride.addVisitorToHistory(visitor7);
-        System.out.println("Visitor " + visitor7.getName() + " in ride history: " + ride.checkVisitorFromHistory(visitor7));
-        ride.printRideHistory();
-        System.out.println("Number of visitors in ride history: " + ride.numberOfVisitors());
+        rideForOperation.addVisitorToHistory(visitor7);
+        System.out.println("Visitor " + visitor7.getName() + " Is it in the ride history: " + rideForOperation.checkVisitorFromHistory(visitor7));
+        rideForOperation.printRideHistory();
+        System.out.println("Number of visitors in the amusement ride history: " + rideForOperation.numberOfVisitors());
     }
 }
+
